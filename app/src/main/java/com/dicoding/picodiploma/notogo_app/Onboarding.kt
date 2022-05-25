e40b3b1f089fdb2d1d2320334a1939ad263d2c13
haniifahfawziyyah <haniifah22fawziyyah@gmail.com>
2022-05-25 12:06:31 +0700

    Onboarding

--- app/src/main/java/com/dicoding/picodiploma/notogo_app/Onboarding.kt
+++ app/src/main/java/com/dicoding/picodiploma/notogo_app/Onboarding.kt
@@ -1,11 +1,70 @@
 package com.dicoding.picodiploma.notogo_app
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.dicoding.picodiploma.notogo_app.databinding.ActivityOnboardingBinding
 
 class Onboarding : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+        playAnimation()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+//            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.signupButton.setOnClickListener {
+//            startActivity(Intent(this, SignupActivity::class.java))
+        }
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val login = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(500)
+        val signup = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(500)
+        val title = ObjectAnimator.ofFloat(binding.titleTextView, View.ALPHA, 1f).setDuration(500)
+        val desc = ObjectAnimator.ofFloat(binding.descTextView, View.ALPHA, 1f).setDuration(500)
+
+        val together = AnimatorSet().apply {
+            playTogether(login, signup)
+        }
+
+        AnimatorSet().apply {
+            playSequentially(title, desc, together)
+            start()
+        }
     }
 }
